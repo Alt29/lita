9cdc178ea954a5579a7a3fffc62d5a19e6a4e354
Alt29 <78425253+Alt29@users.noreply.github.com>
2024-04-09 13:44:26 +0200

    Test hébergement bot

---
+++ a.py
@@ -0,0 +1,48 @@
+import discord
+
+TOKEN = 'MTIyNTE2MjUwNDM5NzY1MjA4MA.GUS3o_.mm1HUBkujEmVBZObMUK53jxngU82TBEjZ4_bfM'
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('!daily'):
+        
+        embed = create_embed('titre', 'desc', discord.Color.red(), 'author_name', 'https://www.manga-city.fr/wp-content/uploads/2022/02/Natsu-Fairy-Tail.jpg', 'https://www.manga-city.fr/wp-content/uploads/2022/02/Natsu-Fairy-Tail.jpg', 'footer', {'1': '2', '3': '4', '5': '6'})
+        await message.channel.send(embed=embed)
+
+def create_embed(title = None, description = None, color = None, author_name = None, author_icon = None, image = None, footer = None, tabFields = None):
+    embed = discord.Embed(
+        title = title,
+        description = description,
+        color = color
+    )
+    
+    if tabFields is not None:
+        for name, value in tabFields.items():
+            embed.add_field(name = name, value = value, inline = False)
+    
+    if image is not None:
+        embed.set_image(url = image)
+    
+    if author_name is not None and author_icon is not None:
+        embed.set_author(name = author_name, icon_url = author_icon)  
+
+    if footer is not None:
+        embed.set_footer(text = footer)
+
+    return embed
+
+client.run(TOKEN)
