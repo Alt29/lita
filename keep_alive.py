cdf96367f0e0fbfc11bbbd771bf40995d4ee0783
Alt29 <78425253+Alt29@users.noreply.github.com>
2024-04-10 23:33:47 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,16 @@
-from flask import Flask
-from threading import Thread
+import http.server
+import socketserver
 
-app = Flask('')
+PORT = 8080
 
-@app.route('/')
-def home():
-    return "I'm alive"
+class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
 
-def run():
-  app.run(host='0.0.0.0',port=8080)
-
-def keep_alive():  
-    t = Thread(target=run)
-    t.start()
+# Créer un serveur qui écoute sur le port 8080
+with socketserver.TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
+    print("Serveur démarré sur le port", PORT)
+    # Lancer le serveur et le maintenir en vie
+    httpd.serve_forever()
