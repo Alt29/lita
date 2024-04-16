7e50f9a3d7a21c555eb32fc4f2583843543367ef
Alt29 <78425253+Alt29@users.noreply.github.com>
2024-04-16 22:40:07 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,16 +1,16 @@
 import http.server
 import socketserver
 
-# PORT = 8080
+PORT = 8080
 
-# class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-#     def do_GET(self):
-#         self.send_response(200)
-#         self.end_headers()
-#         self.wfile.write(b'Hello, world!')
+class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
 
-# # Créer un serveur qui écoute sur le port 8080
-# with socketserver.TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
-#     print("Serveur démarré sur le port", PORT)
-#     # Lancer le serveur et le maintenir en vie
-#     httpd.serve_forever()
+# Créer un serveur qui écoute sur le port 8080
+with socketserver.TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
+    print("Serveur démarré sur le port", PORT)
+    # Lancer le serveur et le maintenir en vie
+    httpd.serve_forever()
