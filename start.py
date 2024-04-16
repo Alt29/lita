e15eb7af4af0842b0ff0cfa2817de1b8cf7b8068
Alt29 <78425253+Alt29@users.noreply.github.com>
2024-04-16 22:46:43 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,11 @@
 import os
 import subprocess
 
+# Exécutez keep_alive.py en arrière-plan
 subprocess.Popen(["python", "keep_alive.py"])
+
+# Exécutez scheduler.py en arrière-plan
+subprocess.Popen(["python", "scheduler.py"])
+
+# Exécutez test.py (ou tout autre script que vous souhaitez exécuter)
 subprocess.call(["python", "test.py"])
